9ea3e6418d13d1580cbc5867ed501e737c590bf6
HakimAbdul12 <abdulhakimaben@gmail.com>
2024-04-25 16:39:37 +0000

    Good Job

--- app.js
+++ app.js
@@ -2,13 +2,33 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 4000;
-const io = require("socket.io");
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(PORT, () => {
+
+const server = app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
 });
-// io.on('connection', (socket) => {
-//     console.log(socket.id)
-// })
+
+//bind socket to the server
+const io = require("socket.io")(server);
+
+io.on('connection', onConnection)
+
+let socketConneced = new Set();
+//run this function when the connection is on
+function onConnection(socket) {
+    console.log(socket.id);
+    socketConneced.add(socket.id);
+    io.emit('clients-total', socketConneced.size);
+    socket.on('disconnect', () => {
+        console.log(socket.id);
+        socketConneced.delete(socket.id);
+        io.emit('clients-total', socketConneced.size);
+    })
+
+    socket.on('message', (data) =>{
+        socket.broadcast.emit('chat-message', data)
+        console.log(data)
+    })
+}
--- public/main.js
+++ public/main.js
@@ -1,1 +1,51 @@
 const socket = io();
+const clientsTotal = document.getElementById("clients-total");
+
+const messageContainer = document.getElementById("message-container");
+const nameInput = document.getElementById("name-input");
+const messageForm = document.getElementById("message-form");
+const messageInput = document.getElementById("message-input");
+
+messageForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  sendMessage();
+});
+
+socket.on("clients-total", (data) => {
+  clientsTotal.innerText = `Total clients: ${data}`;
+});
+
+function sendMessage() {
+  console.log(messageInput.value);
+  const data = {
+    name: nameInput.value,
+    message: messageInput.value,
+    dataTime: new Date(),
+  };
+  socket.emit("message", data);
+  addMessageToUi(true, data)
+  messageInput.value = "";
+}
+
+socket.on("chat-message", (data) => {
+  console.log(data);
+  addMessageToUi(false, data)
+});
+
+function addMessageToUi(isOwn, data) {
+  const element = `
+        <li class="${isOwn? "message-right": "message-left"}">
+            <p class="message">
+                ${data.message}
+                <span>${data.name} 🔯 ${moment(data.dataTime).fromNow()}</span>
+            </p>
+        </li>
+    `;
+    messageContainer.innerHTML += element;
+    scrollTobottom();
+}
+
+//this function automatically scroll you to the bottom when a message comes
+function scrollTobottom(){
+    messageContainer.scrollTo(0, messageContainer.scrollHeight);
+}
